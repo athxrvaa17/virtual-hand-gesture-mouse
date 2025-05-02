baab2bc60da26d564edf06863985bbb25e9226fc
athxrvaa17 <155509992+athxrvaa17@users.noreply.github.com>
2025-05-02 10:17:14 +0530

    Add files via upload

---
+++ mousecontr.py
@@ -0,0 +1,52 @@
+import cv2
+import mediapipe 
+import pyautogui
+from pathlib import Path
+capture_hands = mediapipe.solutions.hands.Hands()
+drawing_option = mediapipe.solutions.drawing_utils
+screen_width , screen_height = pyautogui.size()
+
+camera = cv2.VideoCapture(1)
+x1 = y1 = x2 = y2 = 0
+
+while True:
+    _,image = camera.read()
+    image_height, image_weigth, _ = image.shape
+    image = cv2.flip(image,1)
+    rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    output_hands = capture_hands.process(rgb_image)
+    all_hands = output_hands.multi_hand_landmarks
+    if all_hands:
+        for hand in all_hands :
+            drawing_option.draw_landmarks(image,hand)
+            one_hand_landmarks = hand.landmark
+            for id, lm in enumerate(one_hand_landmarks) :
+                x= int (lm.x * image_weigth)
+                y= int (lm.y * image_height)
+                #print(x,y)
+                if id == 8:
+                    mouse_x = int (screen_width/image_weigth * x )
+                    mouse_y = int (screen_height/image_height * y )
+                    
+                    cv2.circle(image,(x,y),13,(0,255,255))
+                    pyautogui.moveTo(mouse_x,mouse_y)
+                    x1=x
+                    y1=y
+                if id == 4:
+                    x2 = x
+                    y2 = y
+                    cv2.circle(image,(x,y),13,(0,255,255))
+        dist = y2 - y1
+        print(dist)    
+        if(dist<18):
+            pyautogui.click  
+                
+         
+    cv2.imshow("Hand movement video capture",image)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+camera.release()
+cv2.destroyAllWindows()
+
+    
